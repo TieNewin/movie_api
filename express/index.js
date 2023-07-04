21cb2258e3ab032e16c1c00d8f00bc06b1394d94
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-07-04 16:25:14 -0500

    wrote express code to all endpoints and updated documentation.html

--- express/index.js
+++ express/index.js
@@ -5,55 +5,48 @@ const app = express();
 
 app.use(morgan('common'));
 
-let topMovies = [
-    {
-        title: '2001: A Space Odyssey',
-        genre: 'Science Fiction'
-    },
-    {
-        title: 'The Godfather',
-        genre: 'Thriller'
-    },
-    {
-        title: 'Citizen Kane',
-        genre: 'Drama'
-    },
-    {
-        title: 'Raiders of the Lost Ark',
-        genre: 'Action'
-    },
-    {
-        title: "Singin' in the Rain",
-        genre: 'Comedy'
-    },
-    {
-        title: 'Goodfellas',
-        genre: 'Thriller'
-    },
-    {
-        title: 'North by Northwest',
-        genre: 'Thriller'
-    },
-    {
-        title: 'The Dark Knight',
-        genre: 'Action'
-    },
-    {
-        title: 'Beau Travail',
-        genre: 'Drama'
-    },
-    {
-        title: 'The Searchers',
-        genre: 'Action'
-    },
-];
+let movies = [];
 
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-});
+let users = [];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Welcome to my movie app!');
+});
+
+app.get('/movies', (req, res) => {
+    res.send('Successful GET request returning a list of ALL movies');
+});
+
+app.get('/movies/:title', (req, res) => {
+    res.send('Successful GET request returning data on a single movie by title');
+});
+
+app.get('/movies/genres/:genre', (req, res) => {
+    res.send('Successful GET request returning data on a single genre by name');
+});
+
+app.get('/movies/directors/:director', (req, res) => {
+    res.send('Successful GET request returning data on a single director by name');
+});
+
+app.post('/users', (req, res) => {
+    res.send('Successful POST request registering new user');
+});
+
+app.put('/users/:username', (req, res) => {
+    res.send('Successful PUT request updating user\'s username');
+});
+
+app.post('/users/:username/movies/:favorites', (req, res) => {
+    res.send('Successful POST request adding a movie to a user\'s list of favorites');
+});
+
+app.delete('/users/:username/movies/:favorites', (req, res) => {
+    res.send('Successful DELETE request removing a a movie from a user\'s list of favorites');
+});
+
+app.delete('/users/:username', (req, res) => {
+    res.send('Successful DELETE request removing existing user');
 });
 
 app.use((err, req, res, next) => {
