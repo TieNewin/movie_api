1fb15843b4aa101af06e2cb70917b4fa6ce1ffa1
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-07-07 14:49:39 -0500

    changed connection URI to environment variable

--- express/index.js
+++ express/index.js
@@ -33,7 +33,8 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the myFlix App!');
