8be5d20f3bc72013d4ad5bfeceb6ee158b3f90a2
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-07-07 17:06:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ require('./passport');
 //mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the myFlix App!');
+});
+
 /*GET list of all movies*/
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
