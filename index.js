7291788f4c4238d309be3ce7a6947bea5a0bd4df
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-07-11 01:51:31 -0500

    updated allowedorigins array for react app

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234/'];
 
 app.use(cors({
   origin: (origin, callback) => {
